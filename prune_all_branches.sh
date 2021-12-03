00d05c18da575b59b8d3e27ac4fc8e08a584cb26
JuliaMathias <juliamss@gmail.com>
2021-12-03 14:28:31 -0300

    refactor: add g pull to prune_all_branches

--- prune_all_branches.sh
+++ prune_all_branches.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
 git checkout master
-git fetch prune
+git pull
+git fetch --prune
 git branch --merged | egrep -v "(^\*|master|dev)" | xargs git branch -d
 git branch --no-merged | egrep -v "(^\*|master|dev)" | xargs git branch -D
